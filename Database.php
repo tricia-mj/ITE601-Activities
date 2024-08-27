df92ce7385658592119fa26d9e092bf8e980e1c1
tricia-mj <triciamaejabadan@gmail.com>
2024-08-27 13:22:34 +0800

    Defined the ff properties (user, pw, db, host) within Database class

--- Database.php
+++ Database.php
@@ -2,6 +2,12 @@
 
 class Database {
     
+    //Define the following properties within the Database class
+    private $user = 'tricia-mj';
+    private $password = '000_sfumato';
+    private $db = 'ite601';
+    private $host = 'localhost';
+    
 }
 
 ?>
