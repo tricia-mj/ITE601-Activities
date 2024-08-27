5b86ec318ad020f18f85c01e38bbe5a0e2bdca8e
tricia-mj <triciamaejabadan@gmail.com>
2024-08-27 13:32:08 +0800

    Created a method named connect()

--- Database.php
+++ Database.php
@@ -1,13 +1,18 @@
 <?php
 
 class Database {
-    
+
     //Define the following properties within the Database class
     private $user = 'tricia-mj';
     private $password = '000_sfumato';
     private $db = 'ite601';
     private $host = 'localhost';
-    
+
+    // Method that handles the connection to the database
+    public function connect() {
+
+        
+    } 
 }
 
 ?>
